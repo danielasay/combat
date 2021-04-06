a1bf11ef50f6de2fc35c2881ad62aca31adb46c8
Daniel Asay <dasay@daniels-imac.local.dynamic.ucsd.edu>
2021-04-06 12:14:28 -0700

    initial commit

---
+++ combat.R
@@ -0,0 +1,10 @@
+# Script to run ComBat Harmonization on T1 Data for McDonald Lab
+
+# Read in the transposed MRI_all.csv and MRI_info.csv files
+
+data_matrix <- read.csv("/Users/dasay/Dropbox/McDonald_Lab/Data/Output_Spreadsheets/MRI_all_transposed_4.6.2021.csv")
+
+data_info <- read.csv("/Users/dasay/Dropbox/McDonald_Lab/Data/Output_Spreadsheets/MRI_info_3.30.2021.csv")
+
+View(data_matrix)
+View(data_info)
